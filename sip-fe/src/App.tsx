39f2f28451febba01d68cdac13fc4f2a39aea2cb
dangphu2412 <dangphu241299@gmail.com>
2021-11-14 11:12:30 +0700

    Config: axios and sass and bootstap

--- sip-fe/src/App.tsx
+++ sip-fe/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { configAxios } from './config/axios.config';
+
+configAxios();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<ToastContainer />
+			<button onClick={() => toast('Hello dmm')}>Click me</button>
+		</div>
+	);
 }
 
 export default App;
