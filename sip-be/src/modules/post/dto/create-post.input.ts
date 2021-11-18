f3d3c2a3bee301a6d6a820eacb293b4d89fd9d34
dangphu2412 <dangphu241299@gmail.com>
2021-11-18 23:15:35 +0700

    BE/Core: Fix type error in post

--- sip-be/src/modules/post/dto/create-post.input.ts
+++ sip-be/src/modules/post/dto/create-post.input.ts
@@ -3,5 +3,5 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 @InputType()
 export class CreatePostInput {
   @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  id: number;
 }
--- sip-be/src/modules/post/post.resolver.ts
+++ sip-be/src/modules/post/post.resolver.ts
@@ -1,34 +1,34 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { PostService } from './post.service';
-import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { PostType } from './entities/post.type';
 
-@Resolver(() => Post)
+@Resolver(() => PostType)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
-  @Mutation(() => Post)
+  @Mutation(() => PostType)
   createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
     return this.postService.create(createPostInput);
   }
 
-  @Query(() => [Post], { name: 'post' })
+  @Query(() => [PostType], { name: 'post' })
   findAll() {
     return this.postService.findAll();
   }
 
-  @Query(() => Post, { name: 'post' })
+  @Query(() => PostType, { name: 'post' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.postService.findOne(id);
   }
 
-  @Mutation(() => Post)
+  @Mutation(() => PostType)
   updatePost(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
     return this.postService.update(updatePostInput.id, updatePostInput);
   }
 
-  @Mutation(() => Post)
+  @Mutation(() => PostType)
   removePost(@Args('id', { type: () => Int }) id: number) {
     return this.postService.remove(id);
   }
