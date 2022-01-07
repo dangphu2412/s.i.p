b31e360ce39ea818b156c1fe83e2159ab83e44bf
dangphu2412 <dangphu241299@gmail.com>
2022-01-07 22:05:29 +0700

    BE: Seeding votes

--- sip-be-rest/src/app/database/seeds/2-seed-post.seed.ts
+++ sip-be-rest/src/app/database/seeds/2-seed-post.seed.ts
@@ -3,7 +3,7 @@ import { Factory, Seeder } from 'typeorm-seeding';
 
 export default class CreatePosts implements Seeder {
   public async run(factory: Factory): Promise<any> {
-    const POST_COUNT = 50;
+    const POST_COUNT = 100;
     await factory(Post)().createMany(POST_COUNT);
   }
 }
--- sip-be-rest/src/app/database/seeds/3-seed-vote.seed.ts
+++ sip-be-rest/src/app/database/seeds/3-seed-vote.seed.ts
@@ -1,51 +1,25 @@
 import { Post } from '@modules/post/post.entity';
 import { User } from '@modules/user/user.entity';
 import { Vote } from '@modules/vote/vote.entity';
-import { chunk } from 'lodash';
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
-import { random } from 'faker';
 
 export default class CreateVotes implements Seeder {
-  private fromPostToVotes(
-    factory: Factory,
-    { post, author }: { post: Post; author: User },
-  ) {
-    return factory(Vote)({ post, author }).createMany(
-      random.number({ min: 15, max: 100 }),
-    );
-  }
-
   public async run(factory: Factory, connection: Connection): Promise<any> {
-    const allPosts = await connection.getRepository(Post).find();
+    const posts = await connection.getRepository(Post).find();
     const authors = await connection.getRepository(User).find();
-    const chunkedPosts = chunk(allPosts, 10);
 
-    const authorValidateDuplicate = {};
+    const chunkVotes = posts.map((post) => {
+      return authors.map((author) => {
+        const vote = new Vote();
+        vote.author = author;
+        vote.post = post;
+        return vote;
+      });
+    });
 
-    for (const posts of chunkedPosts) {
-      let duplicated = true;
-
-      let author: User;
-
-      while (duplicated) {
-        author = random.arrayElement(authors);
-        if (!authorValidateDuplicate[author.id]) {
-          duplicated = false;
-        } else {
-          authorValidateDuplicate[author.id] = true;
-        }
-      }
-      console.log('Finish random');
-
-      await Promise.all(
-        posts.map((post) =>
-          this.fromPostToVotes(factory, {
-            post,
-            author,
-          }),
-        ),
-      );
+    for await (const votes of chunkVotes) {
+      await connection.getRepository(Vote).insert(votes);
     }
   }
 }
